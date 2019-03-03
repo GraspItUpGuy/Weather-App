beb7b2b64a84229cb244635e60105445f2b918e7
Puneet <birmaan.puneet19@gmail.com>
2019-03-03 12:02:08 +0530

    serving static files and setting post route
    Signed-off-by: Puneet <birmaan.puneet19@gmail.com>

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express')
-
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
 
 
 const  app = express()
 const port  = process.env.PORT || 3000
 
+app.use(bodyParser.urlencoded({extended : true}))
+app.set(express.static(__dirname + './public'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req,res)=>{
-    res.send('this is the home page fellas')
+    res.render('index')
 })
+
+app.post('/', (req,res)=>{
+    res.render('index')
+    console.log(req.body.city)
+})
+
+
 
 app.listen(port, ()=>{
     console.log('server is running at http://localhost:3000')
